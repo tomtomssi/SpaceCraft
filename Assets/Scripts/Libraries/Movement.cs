eb3e18e0b74cc3ba6931d6f1467a9fbac99b2ec1
Tatu Solla <tatu.solla@live.com>
2014-02-27 01:35:50 +0200

    PlayerController lisätty. Movement-pääluokka aloitettu

---
+++ Assets/Scripts/Libraries/Movement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement
+{
+
+    public GameObject player;
+
+    public float verticalSpeed;
+    public float horizontalSpeed;
+
+    public float maxVerticalSpeed;
+    public float maxHorizontalSpeed;
+
+    /// <summary>
+    /// Sets the basic variables for movement
+    /// </summary>
+    /// <param name="target">Target to be moved</param>
+    /// <param name="vs">Target's vertical speed</param>
+    /// <param name="hs">Target's horizontal speed</param>
+    /// <param name="maxVs">Maximum vertical speed</param>
+    /// <param name="maxHs">Maximum horizontal speed</param>
+    public Movement(GameObject target, float vs, float hs, float maxVs, float maxHs)
+    {
+        this.player = target;
+        this.verticalSpeed = vs;
+        this.horizontalSpeed = hs;
+        this.maxHorizontalSpeed = maxHs;
+        this.maxVerticalSpeed = maxVs;
+    }
+
+    /// <summary>
+    /// Sets moves the element to the target direction
+    /// </summary>
+    /// <param name="dir">RIGHT, LEFT, UP, DOWN</param>
+    public void move(string dir)
+    {
+        switch (dir)
+        {
+            case "RIGHT":
+                player.rigidbody2D.AddForce(Vector2.right * verticalSpeed);
+                break;
+            case "LEFT":
+                player.rigidbody2D.AddForce(-Vector2.right * verticalSpeed);
+                break;
+            case "UP":
+                player.rigidbody2D.AddForce(Vector2.up * horizontalSpeed);
+                break;
+            case "DOWN":
+                player.rigidbody2D.AddForce(-Vector2.up * horizontalSpeed);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Movement m;
+
+    public GameObject player;
+
+    public float verticalSpeed;
+    public float horizontalSpeed;
+
+    public float maxVerticalSpeed;
+    public float maxHorizontalSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        m = new Movement(player, verticalSpeed, horizontalSpeed, maxVerticalSpeed, maxHorizontalSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        keyboardInput();
+    }
+
+    void keyboardInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            m.move("UP");
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            m.move("DOWN");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            m.move("LEFT");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            m.move("RIGHT");
+        }
+    }
+}
