e3cc8097528272befc743d0b78e95808a2240b80
Tatu Solla <tatu.solla@live.com>
2014-02-28 19:15:34 +0200

    Tasoa muokattu

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+    public GameObject bgTop;
+    public GameObject bgBot;
+
+    private Vector2 topSpawn;
+
+	// Use this for initialization
+	void Start () {
+        topSpawn = bgTop.transform.position;
+        StartCoroutine("moveBG");
+        Debug.Log(bgBot.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+	}
+
+    public IEnumerator moveBG()
+    {
+        float x = 0.001f;
+        while (true)
+        {
+            bgBot.transform.position = new Vector2(bgBot.transform.position.x, bgBot.transform.position.y + x);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
