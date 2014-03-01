81139978ec45454f25e0c0e88c1c8917af531d9b
Tatu Solla <tatu.solla@live.com>
2014-03-02 00:17:31 +0200

    Vierivän taustakuvan koodi paranneltu

--- Assets/Scripts/Background/GenerateBackground.cs
+++ Assets/Scripts/Background/GenerateBackground.cs
@@ -1,27 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class GenerateBackground : MonoBehaviour {
+public class GenerateBackground : MonoBehaviour
+{
 
     public GameObject backgroundPrefab;
-
-    private int offset = 1;
+    private GameObject backgroundClone;
 
     private float maxY = 1.5f;
-    private float minY = -1.5f; //4
+    private float minY = -1.5f;
 
     private float maxX = 3.75f;
-    private float minX = -3.75f; //8
+    private float minX = -3.75f;
 
-    private List<GameObject> matrix;
-	// Use this for initialization
-	void Start () {
+    void Start()
+    {
         for (int i = 0; i < Mathf.Round(Mathf.Abs(minY) + maxY) + 1; i++)
         {
             for (int j = 0; j < Mathf.Round(Mathf.Abs(minX) + maxX) + 1; ++j)
             {
-                Instantiate(backgroundPrefab, new Vector2(minX + j, minY + i), Quaternion.identity);
-            } 
+                backgroundClone = (GameObject)Instantiate(
+                                                            backgroundPrefab,
+                                                            new Vector2(minX + j, minY + i),
+                                                            Quaternion.identity
+                                                            );
+
+                backgroundClone.transform.parent = GameObject.Find("Background").transform;
+            }
         }
-	}
+    }
 }
