e0b9fbd0c9a545e3586d7c774fcbbc50a2eca064
Tatu Solla <tatu.solla@live.com>
2014-03-01 18:02:22 +0200

    Vierivä taustakuva koodattu paremmin

---
+++ Assets/Scripts/Background/ScrollingBackground.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public float scrollSpeed = .5f;
+    public float offset;
+
+    void Update()
+    {
+        offset += (Time.deltaTime * scrollSpeed) / 10;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
